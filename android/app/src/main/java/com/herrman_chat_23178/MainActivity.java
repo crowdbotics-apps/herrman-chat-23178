fc728c7ce74b162a210e944d5de100912c4bc92b
Crowdbotics <team@crowdbotics.com>
2020-12-03 20:00:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/herrman_chat_23178/MainActivity.java
@@ -0,0 +1,15 @@
+package com.herrman_chat_23178;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "herrman_chat_23178";
+    }
+}
